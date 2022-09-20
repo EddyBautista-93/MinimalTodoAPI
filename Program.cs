30736ceaf9d46e48f9e6f5ab8452e97431934fb5
EddyBautista <bautista.j.eddy@gmail.com>
2022-09-20 18:05:45 -0500

    remove boilerplate code and add nuget package;

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Creates HTTP EndPoint
+app.MapGet("/", () => "Hello World");
+
+app.Run();
